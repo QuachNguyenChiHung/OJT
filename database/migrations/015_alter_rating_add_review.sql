d8384c5ebd7e61048b5e821aa88527b6ab070643
QuachNguyenChiHung <bacon3632@gmail.com>
2025-12-12 13:48:52 +0700

    chore: remove all except OJT_frontendDev

--- database/migrations/015_alter_rating_add_review.sql
+++
@@ -1,4 +0,0 @@
--- Add review and created_at columns to Rating table
-ALTER TABLE Rating 
-ADD COLUMN review TEXT NULL AFTER rating_value,
-ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP AFTER review;
